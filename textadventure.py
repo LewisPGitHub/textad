fa4b88d3a2370998125508d045933b75957dea7c
lewisGithub <77052920+LewisPGitHub@users.noreply.github.com>
2023-11-17 23:15:04 +0000

    Add files via upload

---
+++ textadventure.py
@@ -0,0 +1,9 @@
+inventory = {
+    'key':1,
+
+}
+options = input("what will you do")
+if options == "key" and inventory.get('key') is 1:
+    print("you open the door")
+else:print("you do not open the door")
+
